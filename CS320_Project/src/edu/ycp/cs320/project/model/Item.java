3a5aaf8d4c59ef9c4ac689d93d938f468f6935bf
hgrimm000 <hgrimm@ycp.edu>
2023-04-01 13:44:53 -0400

    Item class changes

--- CS320_Project/src/edu/ycp/cs320/project/model/Item.java
+++ CS320_Project/src/edu/ycp/cs320/project/model/Item.java
@@ -4,26 +4,45 @@ public class Item {
 	
 	private String name;
 	private Boolean canBePickedUp;
+	private int xPositionInRoom;
+	private int yPositionInRoom;
 	
 	public Item() {
 		this.name = "Unnamed Item";
 		this.canBePickedUp = true;
+		this.xPositionInRoom = 0;
+		this.yPositionInRoom = 0;
 	}
 	
-	public Item(String name, Boolean canBePickedUp) {
+	public Item(String name, Boolean canBePickedUp, int xPositionInRoom, int yPositionInRoom) {
 		this.name = name;
 		this.canBePickedUp = canBePickedUp;
+		this.xPositionInRoom = xPositionInRoom;
+		this.yPositionInRoom = yPositionInRoom;
 	}
 	
 	public String getName() {
 		return name;
 	}
 	
-	public void setCanBePickedUp(Boolean canBePickedUp) {
-		this.canBePickedUp = canBePickedUp;
+	public int getXPosition() {
+		return xPositionInRoom;
+	}
+	
+	public int getYPosition() {
+		return yPositionInRoom;
 	}
 	
 	public Boolean getCanBePickedUp() {
 		return canBePickedUp;
+	}
+	
+	public void setCanBePickedUp(Boolean canBePickedUp) {
+		this.canBePickedUp = canBePickedUp;
+	}
+	
+	public void setPosition(int x, int y) {
+		this.xPositionInRoom = x;
+		this.yPositionInRoom = y;
 	}
 }
